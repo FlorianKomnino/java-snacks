89c752972da2ba3c519adcb91fbcbfa4524fd209
FlorianKomnino <florian.komnino@outlook.it>
2023-05-05 15:46:18 +0200

    completed snack9

--- src/org/lessons/java/Snack9.java
+++ src/org/lessons/java/Snack9.java
@@ -1,7 +1,25 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class Snack9 {
 	public static void main(String[] args) {
-		System.out.println("Insert a number for the base ");
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Insert a number of centimeters for the base of te square:");
+		
+		int base = sc.nextInt();
+		
+		System.out.println("Insert a number of centimeters for the height of te square:");
+		
+		int height = sc.nextInt();
+		
+		sc.close();
+		
+		int perimeter = base*2 + height*2;
+		int surface = base*height;
+		
+		System.out.println("The square perimeter with those dimensions is: " + perimeter + "cm");
+		System.out.println("The square surface with those dimensions is: " + surface + "cm2");
 	}
 }
