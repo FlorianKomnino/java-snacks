cd24fa556234f065ac9777598e6e4c455a3df83e
FlorianKomnino <florian.komnino@outlook.it>
2023-05-05 14:29:45 +0200

    Snack4 correction

--- src/org/lessons/java/Snack4.java
+++ src/org/lessons/java/Snack4.java
@@ -7,30 +7,19 @@ public class Snack4 {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Please insert a string to check if it's a palyndrome:");
-		
 		String userInput = sc.nextLine();
-		int strLng = userInput.length();
-		
-		Boolean isPalyndrome = false;
-		
-		int reverseIndex = strLng - 1;
-		
 		sc.close();
 		
-		for (int i=0 ; i<strLng ; i++) {
-			if (userInput.charAt(i) == userInput.charAt(reverseIndex)) {
-				isPalyndrome = true;
-				reverseIndex--;
-			} else {
-				System.out.println("The given string is not a palyndrome");
+		int strLng = userInput.length();
+		
+		for (int i=0 ; i<strLng/2 ; i++) {
+			int revInd = strLng - i - 1;
+			
+			if (userInput.charAt(i) != userInput.charAt(revInd)) {
+				System.out.println("The string is not a palyndrome.");
 				return;
 			}
 		}
-		
-		if (isPalyndrome == true) {
-			System.out.println("Great! The given string is a palyndrome!!");
-		}
-		
-
+		System.out.println("Great! The string is a palyndrome");
 	}
 }
